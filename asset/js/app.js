1ed9bcace502d241a54fd270117fe8e6eeb3389f
yulongzhou1989 <yulongzhou1989@gmail.com>
2017-01-09 13:09:52 -0600

    After first week

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,12 +1,43 @@
 (function (){
   'use strict';
 
-  angular.module('myFirstApp', [])
+  var myFirstApp = angular.module('myFirstApp', []);
 
-  .controller('MyFirstController', function ($scope) {
+  myFirstApp.controller('MyFirstController', MyFirstController);
+  MyFirstController.$inject = ['$scope','$filter'];
+  function MyFirstController($scope, $filter) {
     $scope.name = "Yulong";
     $scope.sayHello = function (){
       return "Zhou";
     }
-  });
+
+    $scope.upper = function (){
+      var upCase = $filter('uppercase');
+      $scope.name = upCase($scope.name);
+    }
+  }
+
+  //angular.module('NameCalculator', [])
+
+  myFirstApp.controller('NameCalculator', NameCalculator);
+  NameCalculator.$inject = ['$scope'];
+  function NameCalculator ($scope){
+    $scope.name = "";
+    $scope.totalValue = 0;
+
+    $scope.displayNumeric = function(){
+      var totalValue = calNumericForStr($scope.name);
+      $scope.totalValue = totalValue;
+    }
+  }
+
+  function calNumericForStr(str){
+    var totalValue = 0;
+
+    for(var i=0; i<str.length; i++){
+      totalValue += str.charCodeAt(i);
+    }
+
+    return totalValue;
+  }
 })();
